8576019a0a88bba2230fa6dcd98ec4d991e21196
panpanhuang332 <135825032+panpanhuang332@users.noreply.github.com>
2025-12-22 01:39:59 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,7 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('ics-store').then((cache) => cache.addAll([
-      'index.html',
-      'manifest.json'
-    ]))
-  );
+const CACHE_NAME = 'ai-lab-v1';
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./'])));
 });
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
